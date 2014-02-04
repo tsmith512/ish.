74d6aba580a4238abd91d30dc8b960180f7d6f09
Taylor Smith <tsmith@tsmithcreative.com>
2014-02-04 05:41:35 +0000

    Strike Google Analytics

--- index.php
+++ index.php
@@ -60,16 +60,5 @@
 <script type="text/javascript" src="js/data-saver.js "></script>
 <script type="text/javascript" src="js/url-handler.js "></script>
 <script type="text/javascript" src="js/init.js "></script>
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-2687475-3']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
 </body>
 </html>
